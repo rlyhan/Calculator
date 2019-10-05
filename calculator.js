0df1d0f7c41857261d002ba5b8e1283f2448e0ea
rlyhan <richard_han@live.com>
2019-10-05 18:48:27 +1300

    Completed

--- calculator.js
+++ calculator.js
@@ -1,15 +1,33 @@
+document.addEventListener('DOMContentLoaded', addButtons)
+
 // Initialise entries as empty array
+entries = [];
 // Initialise total as 0
+total = 0;
 // Initialise current temporary value as empty string
+temp = "";
+// Add event listeners to all buttons
+// Assign variable to input screen
+var buttons;
+var screen;
+function addButtons() {
+    buttons = document.getElementsByTagName('button');
+    for (let i = 0; i < buttons.length; i++) {
+        buttons[i].addEventListener("click", function() {
+            buttonPress(buttons[i].innerText);
+        });
+    }
+    screen = document.querySelector('.form-control');
+}
 // On button click 
-    // Get value from the text of the button
+function buttonPress(symbol) {
     // If button value is a number or '.'
         // Add button value to current temporary value
         // Display answer as the current temporary value's substring of (0, 10)
     // Else if button value is AC
-        // Clear entries, current temporary value and total
+        // Clear entries, current temporary value, total and displayed answer
     // Else if button value is CE
-        // Clear 
+        // Clear current temporary value and displayed answer
     // Else if button value is 'x'
         // Push current temporary value, * symbol to entries 
         // Clear current temporary value
@@ -36,5 +54,48 @@
         // Clear current temporary value
     // Else (button value is %, -, +)
         // Push current temporary value to entries
-        // Push value to entries
+        // Push button value to entries
         // Clear current temporary value
+    if (!isNaN(Number(symbol)) || symbol === '.') {
+        temp += symbol;
+        screen.value = temp.substring(0, 10);
+    } else if (symbol === 'AC') {
+        entries = [];
+        temp = "";
+        total = 0;
+        screen.value = "";
+    } else if (symbol === 'CE') {
+        temp = "";
+        screen.value = "";
+    } else if (symbol === 'x') {
+        entries.push(temp, '*');
+        temp = "";
+    } else if (symbol === '÷') {
+        entries.push(temp, '/');
+        temp = "";
+    } else if (symbol === '=') {
+        entries.push(temp);
+        total = Number(entries[0]);
+        for (let i = 1; i < entries.length; i++) {
+            if (entries[i] === '+') {
+                total += Number(entries[i+1]);
+            } else if (entries[i] === '-') {
+                total -= Number(entries[i+1]);
+            } else if (entries[i] === '*') {
+                total *= Number(entries[i+1]);
+            } else if (entries[i] === '/') {
+                total /= Number(entries[i+1]);
+            }
+            i++;
+        }
+        if (total < 0) {
+            total = '-' + Math.abs(total);
+        }
+        screen.value = total;
+        entries = [];
+        temp = "";
+    } else {
+        entries.push(temp, symbol);
+        temp = "";
+    }
+}
